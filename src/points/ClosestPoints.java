6bbb4019d961d17cd185b1363e3c556859580585
dimon1234 <javaeedev26@gmail.com>
2018-12-10 00:40:31 +0300

    closest points algo for code review

--- src/points/ClosestPoints.java
+++ src/points/ClosestPoints.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class ClosestPoints {
 
@@ -15,60 +16,14 @@ public class ClosestPoints {
             points.add(new Point(scanner.nextLong(), scanner.nextLong()));
         }
 
-        System.out.printf("%.9f", new ClosestPoints().run(points));
+        System.out.printf("%.9f", new ClosestPoints().divideAndConquer(points));
     }
 
-    public double run(List<Point> points) {
-        List<Point> pointsY = new ArrayList<>(points);
-        points.sort(Comparator.comparingLong(Point::getX));
-        return findClosestPoints(points, pointsY, points.size());
-    }
-
-
-    private double findClosestPoints(List<Point> sortedX, List<Point> pointsY, int numberOfPoints) {
-        if (numberOfPoints <= 3) {
-            return minDistanceArea(sortedX, Double.MAX_VALUE);
-        }
-
-        int middleIndex = numberOfPoints / 2;
-        Point middlePoint = sortedX.get(middleIndex);
-
-        List<Point> leftSubPointsY = new ArrayList<>();
-        List<Point> leftSubPointsSortedX = new ArrayList<>();
-        List<Point> rightSubPointsY = new ArrayList<>();
-        List<Point> rightSubPointsSortedX = new ArrayList<>();
-
-        //divide points for left and right subsets
-        for (int i = 0; i < pointsY.size(); i++) {
-            if (pointsY.get(i).getX() <= middlePoint.getX()) {
-                leftSubPointsY.add(pointsY.get(i));
-                leftSubPointsSortedX.add(sortedX.get(i));
-            } else {
-                rightSubPointsY.add(pointsY.get(i));
-                rightSubPointsSortedX.add(sortedX.get(i));
-            }
-        }
-
-        double leftClosest = findClosestPoints(leftSubPointsSortedX, leftSubPointsY, middleIndex);
-        double rightClosest = findClosestPoints(rightSubPointsSortedX, rightSubPointsY, numberOfPoints - middleIndex);
-        double delta = Math.min(leftClosest, rightClosest);
-
-        List<Point> pointsInArea = new ArrayList<>();
-
-        for (int i = 0; i < numberOfPoints; i++) {
-            if (Math.abs(pointsY.get(i).getX() - middlePoint.getX()) < delta) {
-                pointsInArea.add(pointsY.get(i));
-            }
-        }
-
-        return Math.min(delta, minDistanceArea(pointsInArea, delta));
-    }
-
-    private double minDistanceArea(List<Point> pointsInArea, double delta) {
-        double minDistance = delta;
+    private double bruteForce(List<Point> pointsInArea) {
+        double minDistance = Double.MAX_VALUE;
         for (int i = 0; i < pointsInArea.size(); i++) {
             for (int j = i + 1; j < pointsInArea.size(); j++) {
-                if (pointsInArea.get(i).distance(pointsInArea.get(j)) < delta) {
+                if (pointsInArea.get(i).distance(pointsInArea.get(j)) < minDistance) {
                     minDistance = pointsInArea.get(i).distance(pointsInArea.get(j));
                 }
             }
@@ -77,33 +32,74 @@ public class ClosestPoints {
     }
 
 
+    public double divideAndConquer(List<Point> points) {
+        List<Point> pointsSortedByX = new ArrayList<>(points);
+        pointsSortedByX.sort(Comparator.comparingDouble(Point::getX));
+        List<Point> pointsSortedByY = new ArrayList<>(points);
+        pointsSortedByY.sort(Comparator.comparingDouble(Point::getY));
+        return divideAndConquer(pointsSortedByX, pointsSortedByY);
+    }
+
+    private double divideAndConquer(List<Point> pointsSortedByX, List<Point> pointsSortedByY) {
+        int numPoints = pointsSortedByX.size();
+        if (numPoints <= 3)
+            return bruteForce(pointsSortedByX);
+
+        int dividingIndex = numPoints >>> 1;
+        List<Point> leftOfCenter = pointsSortedByX.subList(0, dividingIndex);
+        List<Point> rightOfCenter = pointsSortedByX.subList(dividingIndex, numPoints);
+
+        double closestDistanceLeft = divideAndConquer(leftOfCenter, leftOfCenter.stream()
+                .sorted(Comparator.comparingDouble(Point::getY))
+                .collect(Collectors.toList()));
+
+        double closestDistanceRight = divideAndConquer(rightOfCenter, rightOfCenter.stream()
+                .sorted(Comparator.comparingDouble(Point::getY))
+                .collect(Collectors.toList()));
+
+        List<Point> tempList = new ArrayList<>();
+        double shortestDistance = Math.min(closestDistanceLeft, closestDistanceRight);
+        double centerX = rightOfCenter.get(0).getX();
+        for (Point point : pointsSortedByY)
+            if (Math.abs(centerX - point.getX()) < shortestDistance)
+                tempList.add(point);
+
+        for (int i = 0; i < tempList.size() - 1; i++) {
+            Point point1 = tempList.get(i);
+            for (int j = i + 1; j < tempList.size(); j++) {
+                Point point2 = tempList.get(j);
+                if ((point2.getY() - point1.getY()) >= shortestDistance)
+                    break;
+                double distance = point1.distance(point2);
+                if (distance < closestDistanceLeft) {
+                    closestDistanceLeft = point1.distance(point2);
+                    shortestDistance = distance;
+                }
+            }
+        }
+        return closestDistanceLeft;
+    }
+
+
     public static class Point {
-        private long x;
-        private long y;
+        private final long x;
+        private final long y;
 
         public Point(long x, long y) {
             this.x = x;
             this.y = y;
         }
 
-        public double distance(Point other) {
-            return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
-        }
-
         public long getX() {
             return x;
-        }
-
-        public void setX(long x) {
-            this.x = x;
         }
 
         public long getY() {
             return y;
         }
 
-        public void setY(long y) {
-            this.y = y;
+        public double distance(Point other) {
+            return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
         }
     }
 }
